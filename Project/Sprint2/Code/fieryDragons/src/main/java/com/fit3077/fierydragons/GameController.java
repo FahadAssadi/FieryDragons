21ae48f8d3de4283d82acc34c1b443afb2463d87
Hanif <hanifzafari@gmail.com>
2024-04-27 23:48:59 +1000

    Added JavaFx boiler plate

---
+++ Project/Sprint2/Code/fieryDragons/src/main/java/com/fit3077/fierydragons/GameController.java
@@ -0,0 +1,14 @@
+package com.fit3077.fierydragons;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GameController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to Fiery Dragons Game!");
+    }
+}
---
+++ Project/Sprint2/Code/fieryDragons/src/main/java/com/fit3077/fierydragons/Main.java
@@ -0,0 +1,7 @@
+package com.fit3077.fierydragons;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
