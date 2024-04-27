595f28cfa31949abc059289163dcd95287d9348f
Hanif <hanifzafari@gmail.com>
2024-04-27 23:47:33 +1000

    Added JavaFx boiler plate

---
+++ Project/Sprint2/Code/fieryDragons/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.fit3077.fierydragons {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.fit3077.fierydragons to javafx.fxml;
+    exports com.fit3077.fierydragons;
+}
