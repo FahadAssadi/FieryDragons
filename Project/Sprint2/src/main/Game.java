c7b3ef3a93015758bd94408a2a6bcdec023dda39
Fahad Assadi <fass0001@student.monash.edu>
2024-05-18 14:55:41 +1000

    Added a CreatureKeeper that simply holds the iterators for the Chits Added a test case that tests if the graph is represented accurately

--- Project/Sprint2/src/main/Game.java
+++ Project/Sprint2/src/main/Game.java
@@ -1,11 +1,6 @@
 package main;
 
-import main.game.creature.iterators.CreatureIterator;
-import main.game.creature.iterators.TileableCreatureIterator;
-import main.game.tile.TileNode;
-import main.game.tile.type.CaveTileType;
-import main.game.tile.type.VolcanoTileType;
-
+import main.game.GameBoard;
 
 /**
  * The main.Game class serves as the entry point for the application.
@@ -13,20 +8,12 @@ import main.game.tile.type.VolcanoTileType;
  */
 public class Game {
     public static void main(String[] args) {
-//        // Create the main.game board and the main.ui
-//        GameBoard gB = new GameBoard();
+        // Create the main.game board and the main.ui
+        GameBoard gB = new GameBoard();
 //        GameUI ui = new GameUI(gB);
-//
-//        // Display the main.ui
+
+        // Display the main.ui
 //        ui.displayGameUI();
-
-        CreatureIterator creatureIterator = new CreatureIterator();
-        TileableCreatureIterator tileableCreatureIterator = new TileableCreatureIterator(creatureIterator);
-
-        while (tileableCreatureIterator.hasNext()){
-            System.out.println(tileableCreatureIterator.next().getCreatureName());
-        }
-
     }
 
 }
--- Project/Sprint2/src/main/game/GameBoard.java
+++ Project/Sprint2/src/main/game/GameBoard.java
@@ -1,36 +1,38 @@
 package main.game;
 
-import main.game.chit.DragonCard;
-import main.game.chit.DragonCardManager;
-import main.game.creature.Creature;
-import main.game.creature.CreatureFactory;
-import main.game.event.EventListener;
-import main.game.event.EventManager;
-import main.game.event.EventType;
-import main.game.player.Player;
-import main.game.player.PlayerManager;
-
-import java.util.List;
+import main.game.creature.CreatureKeeper;
+import main.game.tile.TileKeeper;
+import main.game.tile.TileNode;
 
 /**
  * The GameBoard class represents the game board and manages game-related entities.
  * It initializes players, tiles, and dragon cards, and handles player turns.
  */
 public class GameBoard {
+    private final TileKeeper tileKeeper;
 
     /**
      * Constructs a GameBoard object and initializes game-related entities.
      */
     public GameBoard() {
+        CreatureKeeper creatureKeeper = new CreatureKeeper();
+        this.tileKeeper = new TileKeeper(creatureKeeper.getTileableCreatureIterable());
 
+        test();
     }
 
-    /**
-     * Initiates the next player's turn.
-     */
-    public void nextPlayerTurn() {
+    private void test(){
+        for (int i = 0; i < 4; i++){
+            TileNode currPlayerTile = this.tileKeeper.getPlayerTileIterator().next();
+            System.out.println(currPlayerTile.getType().getPlayer());
 
+            TileNode currVolcanoTile = currPlayerTile.getLeft();
+
+            for (int j = 0; j < 6; j++){
+                System.out.println(currVolcanoTile.getTempID());
+                currVolcanoTile = currVolcanoTile.getLeft();
+            }
+        }
     }
-
 
 }
