a8962896a8c0ca0849cf9ce777cf75ed88d421e9
Hanif Mohammad Asif <hmoh0035@student.monash.edu>
2024-06-06 03:43:14 +1000

    Added documentation to VolcanoCard and its iterable

--- Project/Sprint2/src/main/game/tile/VolcanoCard.java
+++ Project/Sprint2/src/main/game/tile/VolcanoCard.java
@@ -4,35 +4,75 @@ import main.misc.Settings;
 
 import java.util.Iterator;
 
+/**
+ * Represents a linked list of volcano cards, each containing a chain of tile nodes.
+ * This class also provides an iterator to traverse through the nodes of a single card.
+ */
 public class VolcanoCard implements Iterable<TileNode>{
     private final TileNode startTileNode;
     private VolcanoCard nextVolcanoCard;
 
+    /**
+     * Constructs a new VolcanoCard with the specified starting tile node.
+     *
+     * @param tileNode the starting node of this volcano card
+     */
     public VolcanoCard(TileNode tileNode) {
         this.startTileNode = tileNode;
     }
 
+    /**
+     * Returns the starting tile node of this volcano card.
+     *
+     * @return the starting tile node
+     */
     public TileNode getStartTileNode() {
         return startTileNode;
     }
 
+    /**
+     * Returns the next volcano card in the linked list.
+     *
+     * @return the next volcano card, or null if there is none
+     */
     public VolcanoCard getNextVolcanoCard() {
         return nextVolcanoCard;
     }
 
+
+    /**
+     * Sets the next volcano card in the linked list.
+     *
+     * @param nextVolcanoCard the next volcano card to set
+     */
     public void setNextVolcanoCard(VolcanoCard nextVolcanoCard) {
         this.nextVolcanoCard = nextVolcanoCard;
     }
 
+    /**
+     * Returns an iterator over the tile nodes in this volcano card.
+     * The iterator traverses nodes starting from the startTileNode and follows their next links.
+     *
+     * @return an iterator of TileNode
+     */
     @Override
     public Iterator<TileNode> iterator() {
         return new volcanoCardIterator();
     }
 
+    /**
+     * Internal iterator to navigate through the TileNodes of a VolcanoCard.
+     */
     private class volcanoCardIterator implements Iterator<TileNode> {
         private TileNode currNode = startTileNode;
         private boolean firstCall = true;
         long totalNumAnimals = (long) Settings.getSetting("VolcanoCardAnimals");
+
+        /**
+         * Determines if there is a next node available in the iteration sequence.
+         *
+         * @return true if there is a next node, false otherwise
+         */
         @Override
         public boolean hasNext() {
             // If it's the first call, we definitely have a next node (the start node)
@@ -63,6 +103,11 @@ public class VolcanoCard implements Iterable<TileNode>{
             return false;
         }
 
+        /**
+         * Returns the next node in the iteration sequence.
+         *
+         * @return the next TileNode
+         */
         @Override
         public TileNode next() {
             if (firstCall) {
--- Project/Sprint2/src/main/game/tile/iterators/VolcanoCardIterable.java
+++ Project/Sprint2/src/main/game/tile/iterators/VolcanoCardIterable.java
@@ -11,16 +11,33 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * Represents an iterable collection of VolcanoCards.
+ * This class also handles the creation and linking of these cards based on provided creatures.
+ */
 public class VolcanoCardIterable implements Iterable<VolcanoCard> {
     private VolcanoCard rootVolcanoCard;
     // Path to the directory containing creature images
     private static final String CREATURE_FILE_PATH = "/assets/pngs/creatures/";
     private static final String DEFAULT_VOLCANO_CARD_CONFIG_PATH = "/data/volcanoCard.json";
 
+    /**
+     * Constructs a VolcanoCardIterable using a provided iterable of creatures.
+     * It initializes the construction of volcano tiles which form the volcano cards.
+     *
+     * @param tileableCreatureIterable an iterable of creatures used to populate the volcano cards
+     */
     public VolcanoCardIterable(TileableCreatureIterable tileableCreatureIterable) {
         constructVolcanoTiles(tileableCreatureIterable);
     }
 
+    /**
+     * Constructs volcano tiles and organizes them into a linked list of VolcanoCards.
+     * Each card is populated using the creatures from the provided iterable.
+     * Images are assigned based on creature names and predefined paths.
+     *
+     * @param tileableCreatureIterable an iterable of creatures used to populate the volcano tiles
+     */
     private void constructVolcanoTiles(TileableCreatureIterable tileableCreatureIterable) {
         long numVolcanoCards = (long) Settings.getSetting("VolcanoCards");
         long totalNumAnimals = (long) Settings.getSetting("VolcanoCardAnimals");
@@ -102,23 +119,44 @@ public class VolcanoCardIterable implements Iterable<VolcanoCard> {
 
     }
 
+    /**
+     * Provides an iterator over the collection of VolcanoCards.
+     *
+     * @return an iterator that allows traversing the linked list of volcano cards
+     */
     @Override
     public Iterator<VolcanoCard> iterator() {
         return new VolcanoCardIterator();
     }
 
+    /**
+     * Iterator for traversing through VolcanoCards in the iterable.
+     */
     private class VolcanoCardIterator implements Iterator<VolcanoCard> {
         private VolcanoCard current;
 
+        /**
+         * Constructs a new VolcanoCardIterator starting with the root volcano card.
+         */
         public VolcanoCardIterator() {
             this.current = rootVolcanoCard;
         }
 
+        /**
+         * Checks if there is another volcano card to return.
+         *
+         * @return true if there is another card, false otherwise
+         */
         @Override
         public boolean hasNext() {
             return current != null;
         }
 
+        /**
+         * Returns the next VolcanoCard in the sequence.
+         *
+         * @return the next VolcanoCard
+         */
         @Override
         public VolcanoCard next() {
             VolcanoCard card = current;
